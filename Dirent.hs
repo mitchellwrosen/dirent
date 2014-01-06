2e76d6a7a429aa964ebbdaa665048f91355d9105
Mitchell Rosen <mitchellwrosen@gmail.com>
2014-01-05 18:15:59 -0800

    refactored ThreadManager to manage IO a rather than IO (), moved all concurrency out of Dirent

--- Dirent.hs
+++ Dirent.hs
@@ -5,70 +5,66 @@ module Dirent
     , makeDirent
     ) where
 
-import           Control.Applicative  ((<$>))
-import           Control.Concurrent   (Chan, forkIO, newChan, readChan, writeChan)
-import           Control.Monad        (liftM, void)
-import           Control.Monad.Trans  (liftIO)
-import           Control.Monad.Writer (WriterT, execWriterT, tell)
-import           Data.Set             (Set)
-import qualified Data.Set             as S
-import           System.Directory     (getDirectoryContents)
-import           System.Posix.Files   (getFileStatus, isDirectory)
-import           System.FilePath      ((</>))
+import           Control.Applicative ((<$>))
+import           Control.Exception   (SomeException)
+import           Control.Monad       (liftM)
+import           Data.Set            (Set)
+import qualified Data.Set            as S
+import           System.Directory    (getDirectoryContents)
+import           System.Posix.Files  (getFileStatus, isDirectory)
+import           System.FilePath     ((</>))
 
-import           ThreadManager        (forkManaged, withManager_)
+import           ThreadManager       (ThreadStatus(..), forkManaged, withManager)
 
-data Dirent a = Directory FilePath (Set (Dirent a))
-              | File FilePath a
+-- A directory entry tagged with some data of type a. An exception could be
+-- thrown trying to tag a dirent, hence the third data constructor.
+data Dirent a = Directory       FilePath (Set (Dirent a))
+              | File            FilePath a
+              | ExceptionThrown SomeException
               deriving Show
 
--- Compare Dirents on their file path; the tag is inconsequential.
+-- Compare Dirents on their file path, the tag is inconsequential.
 instance Eq (Dirent a) where
-    (Directory p1 _) == (Directory p2 _) = p1 == p2
-    (File      p1 _) == (File      p2 _) = p1 == p2
+    (Directory p1 _)     == (Directory p2 _) = p1 == p2
+    (File      p1 _)     == (File      p2 _) = p1 == p2
     _ == _ = False
 
--- Order by file path, alphabetical.
 instance Ord (Dirent a) where
-    (Directory p1 _) <= (Directory p2 _) = p1 <= p2
-    (Directory p1 _) <= (File      p2 _) = p1 <= p2
-    (File      p1 _) <= (Directory p2 _) = p1 <= p2
-    (File      p1 _) <= (File      p2 _) = p1 <= p2
+    (Directory p1 _)     <= (Directory p2 _) = p1 <= p2
+    (Directory p1 _)     <= (File      p2 _) = p1 <= p2
+    (File      p1 _)     <= (Directory p2 _) = p1 <= p2
+    (File      p1 _)     <= (File      p2 _) = p1 <= p2
+    _ <= _ = False
 
+-- User supplied handler does something with a FilePath in IO.
 type Handler a = FilePath -> IO a
 
 makeDirent :: Handler a -> FilePath -> IO (Dirent a)
 makeDirent f path = do
+    -- TODO catch exception
     is_dir <- isDirectory `liftM` getFileStatus path
     if is_dir
         then processDirectory f path
         else processFile f path
 
 processDirectory :: forall a. Handler a -> FilePath -> IO (Dirent a)
-processDirectory f path = do
-    chan <- newChan                                -- Create channel to recieve (Maybe (Dirent a))s on
-    void $ forkIO $ processDirectory' chan         -- Send (Maybe (Dirent a))s and then a Nothing
-    Directory path `liftM` execWriterT (loop chan) -- Write sent (Just (Dirent a))s to children of Directory
+processDirectory f path = Directory path . S.fromList . map threadStatusToDirent <$> processDirectory'
   where
-    processDirectory' :: Chan (Maybe (Dirent a)) -> IO ()
-    processDirectory' chan = do
-        withManager_ $ \threadManager ->
-            getDirectoryContents' path >>= mapM_ (forkManaged threadManager . processPath' chan f)
-        writeChan chan Nothing -- notify parent that all Dirents have been created
+    processDirectory' :: IO [ThreadStatus (Dirent a)]
+    processDirectory' =
+        withManager $ \threadManager -> do
+            getDirectoryContents' path >>= mapM_ (forkManaged threadManager . processPath f)
 
-    -- Read from input channel until Nothing; collect children dirents in a Writer
-    loop :: Chan (Maybe (Dirent a)) -> WriterT (Set (Dirent a)) IO ()
-    loop chan = do
-        val <- liftIO $ readChan chan
-        whenJust val $ \a -> tell (S.singleton a) >> loop chan
+    threadStatusToDirent :: ThreadStatus (Dirent a) -> Dirent a
+    threadStatusToDirent (Finished d) = d
+    threadStatusToDirent (Threw e)    = ExceptionThrown e
+    -- Not reached because threads are waited on in withManager. Should probably refactor
+    -- ThreadStatus for this reason.
+    threadStatusToDirent Running = undefined
 
 -- | Like getDirectoryContents, but prepend the directory name, and remove "." and "..".
 getDirectoryContents' :: FilePath -> IO [FilePath]
 getDirectoryContents' path = map (path </>) . filter (`notElem` [".",".."]) <$> getDirectoryContents path
-
-whenJust :: Monad m => Maybe a -> (a -> m ()) -> m ()
-whenJust Nothing _ = return ()
-whenJust (Just a) f = f a
 
 processFile :: Handler a -> FilePath -> IO (Dirent a)
 processFile f path = File path <$> f path
@@ -77,7 +73,3 @@ processPath :: Handler a -> FilePath -> IO (Dirent a)
 processPath f path = do
     is_dir <- isDirectory <$> getFileStatus path
     if is_dir then processDirectory f path else processFile f path
-
--- Like processPath, but return the dirent on a channel (always Just).
-processPath' :: Chan (Maybe (Dirent a)) -> Handler a -> FilePath -> IO ()
-processPath' chan f path = processPath f path >>= writeChan chan . Just
--- ThreadManager.hs
+++ ThreadManager.hs
@@ -19,23 +19,23 @@ import           Control.Monad      (void)
 import           Data.Map           (Map)
 import qualified Data.Map           as M
 
-data ThreadStatus = Running
-                  | Finished
-                  | Threw SomeException
+data ThreadStatus a = Running
+                    | Finished a
+                    | Threw SomeException
 
-newtype ThreadManager = Mgr (MVar (Map ThreadId (MVar ThreadStatus)))
+newtype ThreadManager a = Mgr (MVar (Map ThreadId (MVar (ThreadStatus a))))
 
-newManager :: IO ThreadManager
+newManager :: IO (ThreadManager a)
 newManager = Mgr `fmap` newMVar M.empty
 
-forkManaged :: ThreadManager -> IO () -> IO ThreadId
+forkManaged :: ThreadManager a -> IO a -> IO ThreadId
 forkManaged (Mgr mgr) action =
     modifyMVar mgr $ \m -> do
         state <- newEmptyMVar
-        tid <- forkIO $ try action >>= putMVar state . either Threw (const Finished)
+        tid <- forkIO $ try action >>= putMVar state . either Threw Finished
         return (M.insert tid state m, tid)
 
-getStatus :: ThreadManager -> ThreadId -> IO (Maybe ThreadStatus)
+getStatus :: ThreadManager a -> ThreadId -> IO (Maybe (ThreadStatus a))
 getStatus (Mgr mgr) tid = do
     modifyMVar mgr $ \m -> do
         case M.lookup tid m of
@@ -44,27 +44,27 @@ getStatus (Mgr mgr) tid = do
                 Nothing     -> return (m, Just Running)
                 Just status -> return (M.delete tid m, Just status)
 
-waitFor :: ThreadManager -> ThreadId -> IO (Maybe ThreadStatus)
+waitFor :: ThreadManager a -> ThreadId -> IO (Maybe (ThreadStatus a))
 waitFor (Mgr mgr) tid = do
     modifyMVar mgr $ \m -> do
         case M.lookup tid m of
             Nothing      -> return (m, Nothing)
             Just mstatus -> fmap ((M.delete tid m,) . Just) (takeMVar mstatus)
 
-waitAll :: ThreadManager -> IO [ThreadStatus]
+waitAll :: ThreadManager a -> IO [ThreadStatus a]
 waitAll (Mgr mgr) = modifyMVar mgr elems >>= mapM takeMVar
   where
     elems = return . (M.empty,) . M.elems
 
-waitAll_ :: ThreadManager -> IO ()
+waitAll_ :: ThreadManager a -> IO ()
 waitAll_ = void . waitAll
 
 -- | Execute an action with a new ThreadManager, and wait on all the launched threads.
-withManager :: (ThreadManager -> IO ()) -> IO [ThreadStatus]
+withManager :: (ThreadManager a -> IO ()) -> IO [ThreadStatus a]
 withManager f = do
     manager <- newManager
     f manager
     waitAll manager
 
-withManager_ :: (ThreadManager -> IO ()) -> IO ()
+withManager_ :: (ThreadManager a -> IO ()) -> IO ()
 withManager_ = void . withManager
